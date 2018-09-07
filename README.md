c5bf4e19149b6341219f0112b5c5cfbde92a3efe
L Lee <lauralee.sh@gmail.com>
2018-09-06 21:27:44 -0700

    updated the final work and README

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Bamazon-Customer
-Bamazon-Customer
+Assignment_Bamazon-Customer
+
+# Link to deployed site
+https://lalatw.github.io/Bamazon-Customer/
+
+
+# Images
+![Bamazon-Customer Assignment](bamazon_customer_demo.gif) 
+
+
+# Technology used
+* MySQL
+* javascript
+* node.js
+* Inquirer
+
+
+# Instruction and npm Install
+* You'll need to run the program in Terminal/Bash using command-line.
+* Please run npm Install for package needed as listed below.
+* Populate database first by running bamazon.sql in MySQL Workbench or other available tools.
+* Run "bamazonCustomer.js" in Terminal/Bash to start the program.
+
+```
+  "dependencies": {
+    "cli-table": "^0.3.1",
+    "inquirer": "^6.2.0",
+    "mysql": "^2.16.0"
+  }
+```
+
+# Code snippets
+
+![code_sniippets](code_screenshot.png) 
+
+
+# Learning points
+* Use MySQL to store data and query database.
+* Create database and read data in need. 
+* Apply Node.js to run backend and executes JavaScript code outside of a browser. 
+* Run npm install to use pre-built packages of modules.
+* Use Require to grab data from sources.
+
+
+
+# Author 
+[Shuhan Laura Lee](https://lalatw.github.io/Bamazon-Customer/)
+
+
+
+# License
+Standard MIT License
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,14 +16,16 @@ connection.connect(function (err) {
 
 display();
 
+
+// function to select table from database and display table using cli-table.
 function display() {
     connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
         var table = new Table({
             head: ["ID", "Product Name", "Department", "Price", "Stock Qty"],
             colWidths: [6, 45, 16, 11, 11]
         });
     
-        // table is an Array, so you can `push`, `unshift`, `splice`
         for (var i = 0; i < res.length; i++) {
             table.push(
                 [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity],
@@ -34,7 +36,7 @@ function display() {
     });
 };
 
-
+// function to prompt questions and process order.
 function start() {
     inquirer.prompt([
         {
@@ -42,10 +44,13 @@ function start() {
             name: "itemID",
             message: "What is the ID of the item you would like to purchase? [Quit with Q]",
             validate: function(inputID) {
+                //if users enter q or Q, exit the program.
                 if (inputID.toLowerCase() === "q") {
+                    connection.end();
                     process.exit();
                 }
-                else if (!isNaN(inputID)) {
+                //users need to enter a number between 1 to 10 because there are 10 items in the table.
+                else if ((!isNaN(inputID)) && (inputID<=10) && (inputID>0)) {
                     return true;
                 }
                 console.log("Please enter a valid ID.");
@@ -57,9 +62,12 @@ function start() {
             name: "quantity",
             message: "How many would you like? [Quit with Q]",
             validate: function(inputQ) {
+                //if users enter q or Q, exit the program.
                 if (inputQ.toLowerCase() === "q") {
+                    connection.end();
                     process.exit();
                 }
+                //users need to enter a number
                 else if (!isNaN(inputQ)) {
                     return true;
                 }
@@ -71,7 +79,7 @@ function start() {
 
         connection.query("SELECT * FROM products", function(err, res) {
             if (err) throw err;
-     
+            //query through out the table to find the matched ID as the selected item.
             var chosenItem ="";
               for (var i = 0; i < res.length; i++) {
                 if (res[i].item_id === parseInt(answer.itemID)) {
@@ -79,12 +87,14 @@ function start() {
                 }
               }
               
+              //check if the inventory has enough of the product to meet the customer's request.
+              //if it has enough of the product, update the SQL database to reflect the remaining quantity
               if (chosenItem.stock_quantity >= parseInt(answer.quantity)) {
                 connection.query(
                   "UPDATE products SET ? WHERE ?",
                   [
                     {
-                      stock_quantity: chosenItem.stock_quantity-=answer.quantity
+                      stock_quantity: chosenItem.stock_quantity-= parseInt(answer.quantity)
                     },
                     {
                       item_id: chosenItem.item_id
@@ -92,6 +102,7 @@ function start() {
                   ],
                   function(error) {
                     if (error) throw err;
+                    //Once the update goes through, show the customer the total cost of their purchase.
                     var totalCost = answer.quantity * chosenItem.price;
                     console.log("Thank you. Successfully purchased "+ answer.quantity + " " + chosenItem.product_name + ".");
                     console.log("Your total cost is $"+ totalCost + ".");
@@ -100,6 +111,7 @@ function start() {
                 );
               }
               else {
+                //show error message when inventory is not sufficient to prevent the order from going through.
                 console.log("Sorry. Insufficient quantity! Please select another item or change your quantity.");
                 start();
               }
